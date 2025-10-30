d65f4eb4686d42ca4fbeac43a526d767ee6e1f42
sameerkindia <sameerkhanaa5@gmail.com>
2025-10-30 09:34:01 +0530

    checkout layout

---
+++ src/app/checkout/_layout.tsx
@@ -0,0 +1,16 @@
+import { Stack } from "expo-router";
+import CheckoutFormProvider from "../../context/CheckoutFormProvider";
+import CheckoutFormStepIndicator from "../../components/CheckoutFormStepIndicator";
+
+export default function RootLayout() {
+  return (
+    <CheckoutFormProvider>
+      <CheckoutFormStepIndicator />
+      <Stack screenOptions={{headerShown: false}}>
+        <Stack.Screen name="personal" options={{ title: "Personal" }} />
+        <Stack.Screen name="payment" options={{ title: "Payment" }} />
+        <Stack.Screen name="confirm" options={{ title: "Confirm" }} />
+      </Stack>
+    </CheckoutFormProvider>
+  );
+}
